8ddf08e230792eaf10c95d8d7873ba1fd8e059c9
shubham2637 <shubham37@outlook.com>
2018-10-14 11:33:46 +0530

    Added Donor and Collector tables

--- dbms/Medical_Donation/admin.py
+++ dbms/Medical_Donation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Collector, Doner 
 # Register your models here.
+
+admin.site.register(Doner)
+admin.site.register(Collector)
+#admin.site.register(MedicineAcceptor)
--- dbms/Medical_Donation/models.py
+++ dbms/Medical_Donation/models.py
@@ -44,3 +44,28 @@ class Collector(models.Model):
     username = models.CharField(max_length=16,unique=True)
     password = models.CharField(max_length=16)
     image = models.ImageField(upload_to=get_image_path, blank =True, null=True)
+
+
+    def __str__(self):
+        return (f"{self.name} {self.address} {self.username}")
+
+
+    class MedicineAcceptor(models.Model):
+        #to store a Image file
+        def get_image_path(instance, filename):
+            return os.path.join('photos', str(instance.id), filename)
+
+        name = models.CharField(max_length=64)
+        proprietor = models.CharField(max_length=64)
+        license_no = models.CharField(max_length=64)
+        address = models.CharField(max_length=128)
+        pincode = models.PositiveIntegerField(max_length=6)
+        email = models.EmailField()
+        username = models.CharField(max_length=64, unique=True)
+        password = models.CharField(max_length=16)
+        Phone_no = models.IntegerField(max_length=10)
+        image = models.ImageField(upload_to=get_image_path, blank =True, null=True)
+
+
+        def __str__(self):
+            return (f"{self.name} {self.address} {self.username}")
